f467bf1a50ef2c067c720223ed4a7792ad725680
Rahul Gaur <42992725+CoderWalk@users.noreply.github.com>
2025-10-28 21:26:28 +0530

    tuesday commit1

--- first.js
+++ first.js
@@ -2,5 +2,7 @@ let a=10
 let b=28
 let c=a+b
 console.log(`sum of ${a} & ${b} is ${c}`)
-console.log("result=",c)
+console.log("result=",c);
+  console.log("i am added now");
+
 //sum of 10 & 20 is 30
